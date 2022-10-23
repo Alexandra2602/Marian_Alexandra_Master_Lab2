3b865686f659b8618774a2caec7062e9fd66596c
marian alexandra <marianalexandra2602@yahoo.com>
2022-10-23 23:32:59 +0300

    Add project files.

---
+++ Marian_Alexandra_Lab2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Marian_Alexandra_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
